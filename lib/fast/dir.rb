a90c77266609a9583dc15fb86f3876dd905d3e29
Xavier Via <fernando.via@gmail.com>
2011-11-13 07:26:24 +0100

    Fast::Dir in process

--- lib/fast/dir.rb
+++ lib/fast/dir.rb
@@ -1,16 +1,44 @@
 module Fast
   # Directory handling class
-  class Dir
-    # .call -> like Fast::File.call
+  class Dir < Array
+    # .call -> like Fast::File.call -> NOUP: make delegator pattern Singleton-like
     
-    # #list
+    # Returns a Fast::Dir list with all items in the directory, except ".." and "."
+    def list path = nil
+      @path = normalize path if path
+      ::Dir.foreach @path do |entry|
+        self << entry unless entry == "." or entry == ".."
+      end
+      self
+    end
     
-    # #files
+    # Returns a Fast::Dir list of all files in the directory. Non recursive 
+    # (at least yet)
+    def files path = nil
+      @path = normalize path if path
+      ::Dir.foreach @path do |entry|
+        self << entry unless ::File.directory? "#{@path}/#{entry}"
+      end
+      self
+    end
     
-    # #dirs
-    
+    # Returns a Fast::Dir list of all directories in the directory, non-recursive
+    # and excluding points
+    def dirs path = nil
+      @path = normalize path if path
+      ::Dir.foreach @path do |entry|
+        self << entry if ::File.directory? "#{@path}/#{entry}" and entry != "." and entry != ".."
+      end
+      self
+    end
+
     # #create
   
     # #exist?
+    
+    private
+      def normalize path
+        @path = "#{path}"
+      end
   end
 end
--- spec/fast/dir_spec.rb
+++ spec/fast/dir_spec.rb
@@ -2,22 +2,77 @@ require "fast"
 
 describe Fast::Dir do
   describe "#list" do 
-    it "should return a list of all items in the directory"
+    it "should return a list of all items in the directory, excluding '.' & '..'" do
+      ::File.should_not be_directory "demo"
+      ::Dir.mkdir "demo"
+      Fast::File.new.touch "demo/alice.txt"
+      Fast::File.new.touch "demo/betty.txt"
+      Fast::File.new.touch "demo/chris.txt"
+      ::Dir.mkdir "demo/subdir"
+      
+      Fast::Dir.new.list( "demo" ).should include "alice.txt"
+      Fast::Dir.new.list( "demo" ).should include "betty.txt"
+      Fast::Dir.new.list( "demo" ).should include "chris.txt"
+      Fast::Dir.new.list( "demo" ).should include "subdir"
+      Fast::Dir.new.list( "demo" ).should_not include ".."
+      Fast::Dir.new.list( "demo" ).should_not include "."
+    
+      ::Dir.unlink "demo/subdir"
+      ::File.unlink "demo/chris.txt"
+      ::File.unlink "demo/betty.txt"
+      ::File.unlink "demo/alice.txt"
+      ::Dir.unlink "demo"
+    end
   end
   
   describe "#files" do
-    it "should return a list of all files in the directory"
-    
-    context "args :extension => 'txt'" do
-      it "should return a list of all files with .txt as extension in the directory"
+    it "should return a list of all files in the directory" do
+      ::File.should_not be_directory "demo"
+      ::Dir.mkdir "demo"
+      Fast::File.new.touch "demo/alice.txt"
+      Fast::File.new.touch "demo/betty.txt"
+      Fast::File.new.touch "demo/chris.txt"
+      ::Dir.mkdir "demo/subdir"
+      
+      Fast::Dir.new.files( "demo" ).should include "alice.txt"
+      Fast::Dir.new.files( "demo" ).should include "betty.txt"
+      Fast::Dir.new.files( "demo" ).should include "chris.txt"
+      Fast::Dir.new.files( "demo" ).should_not include "subdir"
+      Fast::Dir.new.files( "demo" ).should_not include ".."
+      Fast::Dir.new.files( "demo" ).should_not include "."
+        
+      ::Dir.unlink "demo/subdir"
+      ::File.unlink "demo/chris.txt"
+      ::File.unlink "demo/betty.txt"
+      ::File.unlink "demo/alice.txt"
+      ::Dir.unlink "demo"
     end
   end
   
   describe "#dirs" do
-    it "should return a list containing all dirs in the directory"
-    
-    context "args :skip => :dots" do
-      it "should return a list of dirs excluding '.' and '..'"
+    it "should return a list containing all dirs in the directory, excludind dots" do
+      ::File.should_not be_directory "demo"
+      ::Dir.mkdir "demo"
+      Fast::File.new.touch "demo/alice.txt"
+      Fast::File.new.touch "demo/betty.txt"
+      Fast::File.new.touch "demo/chris.txt"
+      ::Dir.mkdir "demo/subdir"
+      ::Dir.mkdir "demo/endodir"
+      
+      Fast::Dir.new.dirs( "demo" ).should_not include "alice.txt"
+      Fast::Dir.new.dirs( "demo" ).should_not include "betty.txt"
+      Fast::Dir.new.dirs( "demo" ).should_not include "chris.txt"
+      Fast::Dir.new.dirs( "demo" ).should include "subdir"
+      Fast::Dir.new.dirs( "demo" ).should include "endodir"
+      Fast::Dir.new.dirs( "demo" ).should_not include ".."
+      Fast::Dir.new.dirs( "demo" ).should_not include "."
+        
+      ::Dir.unlink "demo/subdir"
+      ::Dir.unlink "demo/endodir"
+      ::File.unlink "demo/chris.txt"
+      ::File.unlink "demo/betty.txt"
+      ::File.unlink "demo/alice.txt"
+      ::Dir.unlink "demo"
     end
   end
   
